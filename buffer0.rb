3182ba648da7a598fe48bae3c3cd3323dfc4e0ba
Nestore "Coinstable" Locarno <50032782+coinstable@users.noreply.github.com>
2019-04-27 08:37:43 +0200

    Create buffer0.rb

---
+++ buffer0.rb
@@ -0,0 +1,53 @@
+#------------------------------------------------|
+#                                                |
+# ALCE                                           |
+# Audio Live-Coding Environment!                 |
+#                                                |
+# Thanks to James McCartney's SuperCollider,     |
+# Sam Aaron's Sonic-Pi,                          |
+# Louis Pilfold's Sonic-Pi-Tool,                 |
+# Nick Johnstone's Sonic-Pi-Cli,                 |
+#                                                |
+# Coded by Nestore Locarno                       |
+# https://github.com/coinstable/alce             |
+#                                                |
+#------------------------------------------------|         
+
+live_loop :buffer do
+
+##| "Music = Code, Code = Art." 
+##| Sam Aaron
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
