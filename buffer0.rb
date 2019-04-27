cbc65e3b1c236cc6a8553761150f76616dae97ab
Nestore "Coinstable" Locarno <50032782+coinstable@users.noreply.github.com>
2019-04-27 09:28:36 +0200

    Update buffer0.rb

--- buffer0.rb
+++ buffer0.rb
@@ -15,9 +15,26 @@
 
 live_loop :buffer do
 
-##| "Music = Code, Code = Art." 
-##| Sam Aaron
-
+##| Comandi IDE:
+  # M-f-s  =  salva buffer
+  # M-f-o  =  carica buffer
+  # M-y-h  =  apri shell window
+  # M-y-s  =  inserisci comando sh
+  # M-i-t  =  cambia contenuto finestra
+  # M-i-d  =  elimina finestra
+  # M-i-o  =  seleziona finestra successiva
+##| Comandi Editor:
+  # M-e-b  =  inizia/interrompi selezione testo
+  # M-e-c  =  taglia selezione testo
+  # M-e-o  =  copia selezione testo
+  # M-e-p  =  incolla selezione
+  # M-e-g-c  =  commenta selezione testo
+  # M-e-g-n  =  decommenta selezione testo
+##| Comandi Shell:
+  # ./alce-log  =  attiva la finestra di log
+  # ./P  =  esegui buffer
+  # ./S  =  ferma esecuzione
+  
 end
 
 
